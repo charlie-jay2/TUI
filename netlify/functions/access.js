28f99f47a23644c36480fb5771fef7a2cf2bca10
FlexaGroup <162819959+FlexaGroup@users.noreply.github.com>
2025-04-05 02:05:30 +0100

    upd

--- netlify/functions/access.js
+++ netlify/functions/access.js
@@ -3,7 +3,7 @@ exports.handler = async function (event, context) {
     const { code } = JSON.parse(event.body);
 
     // Define a valid access code
-    const validAccessCode = "1234567890"; // Replace with your actual access code
+    const validAccessCode = "1983059239"; // Replace with your actual access code
 
     if (code === validAccessCode) {
         return {
